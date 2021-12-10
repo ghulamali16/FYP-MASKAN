ecf316a89a72e5a3f134aa438727468c688631b9
ghulamali16 <ghulam.ali@devsinc.com>
2021-12-11 00:02:19 +0500

    App changes for images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
-
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: 'sfnkjdnmksnksnnsfndm',
-    aws_secret_access_key: 'jdanjnsdjnajsdndnjas'
-  }
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIAYNOUZSEUKOIPRY5S',
+      aws_secret_access_key: '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',
+      region:'us-east-2'
+    }
   config.storage = :fog
   config.permissions = 0o666
   config.cache_dir = "#{Rails.root}/tmp/"
-  config.fog_directory = ENV['FOG_DIRECTORY']
+  config.fog_directory = "#{Rails.root}/public/"
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
 end
